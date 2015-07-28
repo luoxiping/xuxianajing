1cf48f8f75cdb52ede0a20733f7ac98f4e4041a3
luoxp <luoxp@jugame.cn>
2015-07-28 19:05:10 +0800

    发布功能

--- src/com/example/xuxianjing/activity/SendMessageActivity.java
+++ src/com/example/xuxianjing/activity/SendMessageActivity.java
@@ -1,24 +1,31 @@
 package com.example.xuxianjing.activity;
 
 import com.example.xuxianjing.R;
-
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 public class SendMessageActivity extends BaseActivity {
 	private EditText mEditText;
-	private Button mBtn;
+	private ImageView mImageView;
 
 	@Override
 	public void initWidget() {
 		setContentView(R.layout.activity_send_message);
+		mEditText = (EditText) findViewById(R.id.edit);
+		mImageView = (ImageView) findViewById(R.id.imageview);
 	}
 
 	@Override
 	public void widgetClick(View v) {
+		switch (v.getId()) {
+		case R.id.add_btn:
+			
+			break;
 
+		default:
+			break;
+		}
 	}
 
 }
