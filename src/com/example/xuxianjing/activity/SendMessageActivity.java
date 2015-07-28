0d8d5b5b9e16739d6a32af2dc3ec6e16213f0df7
luoxp <luoxp@jugame.cn>
2015-07-28 18:52:25 +0800

    添加分享功能

---
+++ src/com/example/xuxianjing/activity/SendMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.xuxianjing.activity;
+
+import com.example.xuxianjing.R;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class SendMessageActivity extends BaseActivity {
+	private EditText mEditText;
+	private Button mBtn;
+
+	@Override
+	public void initWidget() {
+		setContentView(R.layout.activity_send_message);
+	}
+
+	@Override
+	public void widgetClick(View v) {
+
+	}
+
+}
